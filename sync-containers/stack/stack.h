2b4f20d405c99346e068e376910164cfbb53e9bf
Konstantin Morozov <just.morozov.k@gmail.com>
2024-03-21 09:07:30 +0100

    add stack

---
+++ sync-containers/stack/stack.h
@@ -0,0 +1,30 @@
+//
+// Created by konstantin on 21.03.24.
+//
+
+#pragma once
+
+#include <atomic>
+
+#include <simple/hazard_ptr.h>
+
+namespace NSync {
+
+template <typename T>
+
+class Stack {
+public:
+    Stack() = default;
+    void Clear() {}
+    void Push(const T& value) {}
+    bool Pop(T* value) {}
+private:
+    struct Node {
+        T value;
+        Node* next;
+    };
+
+    std::atomic<Node*> head_{};
+};
+
+}  // namespace NSync
